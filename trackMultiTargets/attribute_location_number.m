56323b6dc54440d91d408130bae5b783b04d1a0e
Shen-kl <1003783491@qq.com>
2024-07-17 17:19:23 +0800

    fix the bug when attributing the location number and sudokuing

--- trackMultiTargets/attribute_location_number.m
+++ trackMultiTargets/attribute_location_number.m
@@ -5,7 +5,7 @@ function [zone_number]=attribute_location_number(PolarX, distanceStep, aziStep, 
     %input  PolarX 极坐标系下的坐标
     %input  rangeStep 距离上的分区步长
     %input  aziStep 方位角上的分区步长
-    %output number 分区编号 从0开始
+    %output number 分区编号 从1开始
     
     R=PolarX(1);
     azi = PolarX(2); % 180 ~ 180
